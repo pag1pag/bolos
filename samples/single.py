6964def3ac77e276b8fd5f220798662717764261
Pierre-Antoine Goutier <pierre-antoine.goutier@student-cs.fr>
2022-10-03 18:15:05 +0200

    Add a test (taken from an other branch of bolos)

--- samples/single.py
+++ samples/single.py
@@ -13,41 +13,42 @@ the Boltzmann equation for E/n = 120 Td and a gas temperature of
 
 """
 
-import sys
+
 import logging
 import argparse
 
-import numpy as np
+import pylab
 import scipy.constants as co
 from bolos import parser, solver, grid
+
 
 def parse_args():
     argparser = argparse.ArgumentParser()
 
-    argparser.add_argument("input", 
-                        help="File with cross-sections in BOLSIG+ format")
-    argparser.add_argument("--debug", 
-                        help="If set, produce a lot of output for debugging", 
-                        action='store_true', default=False)
-    argparser.add_argument("--en", 
-                           help="Reduced field (in Td)", 
+    argparser.add_argument("input",
+                           help="File with cross-sections in BOLSIG+ format")
+    argparser.add_argument("--debug",
+                           help="If set, produce a lot of output for debugging",
+                           action='store_true', default=False)
+    argparser.add_argument("--en",
+                           help="Reduced field (in Td)",
                            type=float, default=100.0)
-    argparser.add_argument("--temp", "-T", 
-                           help="Gas temperature (in K)", 
+    argparser.add_argument("--temp", "-T",
+                           help="Gas temperature (in K)",
                            type=float, default=300.0)
 
     args = argparser.parse_args()
     return args
+
 
 def main():
     args = parse_args()
 
     if args.debug:
         logging.basicConfig(format='[%(asctime)s] %(module)s.%(funcName)s: '
-                            '%(message)s', 
+                            '%(message)s',
                             datefmt='%a, %d %b %Y %H:%M:%S',
                             level=logging.DEBUG)
-
 
     # Use a linear grid from 0 to 60 eV with 500 intervals.
     gr = grid.LinearGrid(0, 60., 500)
@@ -57,7 +58,7 @@ def main():
 
     # Parse the cross-section file in BOSIG+ format and load it into the
     # solver.
-    with open(args.input) as fp:
+    with open(args.input, "r", encoding="utf-8") as fp:
         bsolver.load_collisions(parser.parse(fp))
 
     # Set the conditions.  And initialize the solver
@@ -85,24 +86,23 @@ def main():
 
     # Search for a particular process in the solver and print its rate.
     #k = bsolver.search('N2 -> N2^+')
-    #print "THE REACTION RATE OF %s IS %g\n" % (k, bsolver.rate(f1, k))
-    
+    # print "THE REACTION RATE OF %s IS %g\n" % (k, bsolver.rate(f1, k))
+
     # You can also iterate over all processes or over processes of a certain
     # type.
-    print "\nREACTION RATES:\n"
-    for t, p in bsolver.iter_all():
-        print "%-40s   %.3e m^3/s" % (str(p), bsolver.rate(f1, p))
+    print("\nREACTION RATES:\n")
+    for _, p in bsolver.iter_all():
+        print(f"{str(p)}  {bsolver.rate(f1, p)} m^3/s")
 
     # Calculate the mobility and diffusion.
-    print "\nTRANSPORT PARAMETERS:\n"
-    print "mobility * N   = %.3e  1/m/V/s" % bsolver.mobility(f1)
-    print "diffusion * N  = %.3e  1/m/s" % bsolver.diffusion(f1)
-    print "average energy = %.3e  eV" % bsolver.mean_energy(f1)
+    print("\nTRANSPORT PARAMETERS:\n")
+    print(f"mobility * N   = {bsolver.mobility(f1)} 1/m/V/s")
+    print(f"diffusion * N  = {bsolver.diffusion(f1)} 1/m/s")
+    print(f"average energy = {bsolver.mean_energy(f1)} eV")
 
-    import pylab
     pylab.plot(bsolver.grid.c, f1)
     pylab.show()
-    
+
 
 if __name__ == '__main__':
     main()
---
+++ samples/validation.py
@@ -0,0 +1,53 @@
+from os.path import abspath, dirname, join
+import scipy.constants as co
+import matplotlib.pyplot as plt
+from bolos import parser, solver, grid
+
+mechanism = join(dirname(abspath(__file__)),  "itikawa-2009-O2.txt")
+print(mechanism)
+
+
+def main():
+    # Use a linear grid from 0 to 60 eV with 500 intervals.
+    gr = grid.LinearGrid(0.001, 60, 100)
+    # gr = grid.GeometricGrid(0.1, 60, 100)
+
+    # Initiate the solver instance
+    bsolver = solver.BoltzmannSolver(gr)
+
+    # Parse the cross-section file in BOSIG+ format and load it into the
+    # solver.
+    with open(mechanism, "r", encoding="utf-8") as fp:
+        bsolver.load_collisions(parser.parse(fp))
+
+    # Set the conditions.  And initialize the solver
+    T = 1000  # Gas temperature, K
+    P = 101325  # pression, Pa
+    ND = P / co.k / T  # densité, m^-3
+    bsolver.target['O2'].density = 1.0  # molar fraction, -
+    bsolver.kT = T * co.k / co.eV  # Gas temperature, eV
+    E = 1e5  # Electric field, V
+    bsolver.EN = E / ND  # * solver.TOWNSEND  # Reduced electric field, Td
+    bsolver.init()
+
+    # Start with Maxwell EEDF as initial guess.  Here we are starting with
+    # with an electron temperature of 2 eV
+    f0 = bsolver.maxwell(2.0)
+
+    # Solve the Boltzmann equation with a tolerance rtol and maxn iterations.
+    f1 = bsolver.converge(f0, maxn=200, rtol=1e-5)
+
+    # Calculate the properties.
+    print(f"mobility = {bsolver.mobility(f1) / ND:.3f} 1/m/V/s")
+    print(f"diffusion = {bsolver.diffusion(f1) / ND:.3f} 1/m/s")
+    print(f"average energy = {bsolver.mean_energy(f1):.3f} eV")
+    # print("electron temperature = %.3f K" % bsolver.electron_temperature(f1))
+
+    plt.plot(bsolver.cenergy, f0, label="f0")
+    plt.plot(bsolver.cenergy, f1, label="f0")
+    plt.legend()
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
